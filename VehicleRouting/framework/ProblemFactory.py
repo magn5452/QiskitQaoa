b64a45dd8a14e00032047a6a6b9cfc94a164e818
Magnus <magnuslinnetmadsen@gmail.com>
2021-09-09 07:56:25 +0200

    Created a factory for the problem class

---
+++ VehicleRouting/framework/ProblemFactory.py
@@ -0,0 +1,18 @@
+from abc import abstractmethod, ABC
+
+
+class VehicleRoutingProblemStrategy:
+    @abstractmethod
+    def create_graph(self):
+        pass
+
+    @abstractmethod
+    def create_penalty_factor(self):
+        pass
+
+    @abstractmethod
+    def create_number_of_vehicles(self):
+        pass
+
+
+
